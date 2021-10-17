9af9a4e768715e3807ad50faba2dd4c83eb0b34f
GhexterCortes <69035887+GhexterCortes@users.noreply.github.com>
2021-10-17 20:22:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-//    ###      ##     ##   ####    ######  
-//   ## ##      ##   ##     ##    ##    ## 
-//  ##   ##      ## ##      ##    ##       
-// ##     ##      ###       ##     ######  
-// #########     ## ##      ##          ## 
-// ##     ##    ##   ##     ##    ##    ## 
-// ##     ##   ##     ##   ####    ######  
+/**    ###      ##     ##   ####    ######  
+      ## ##      ##   ##     ##    ##    ## 
+     ##   ##      ## ##      ##    ##       
+    ##     ##      ###       ##     ######  
+    #########     ## ##      ##          ## 
+    ##     ##    ##   ##     ##    ##    ## 
+    ##     ##   ##     ##   ####    ######  
+**/
 
 // Modules
 const Startup = require('./scripts/startup')();
 const Logger = require('./scripts/logger');
 const Config = require('./scripts/config');
-const prompt = require('prompt-sync')();
 
+const log = new Logger();
 const parseConfig = new Config();
 parseConfig.location = './config/config.yml';
 parseConfig.parse();
 parseConfig.testmode();
 parseConfig.prefill();
 
+// Functions
 console.log(parseConfig.config);
