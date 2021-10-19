cacd50326ee73283160bd5580f7b40f2b21f06b7
GhexterCortes <69035887+GhexterCortes@users.noreply.github.com>
2021-10-19 19:53:02 +0800

    Language.yml

--- index.js
+++ index.js
@@ -12,10 +12,63 @@ require('./scripts/startup')();
 // Modules
 const Util = require('fallout-utility');
 const Config = require('./scripts/config');
+const Language = require('./scripts/language');
+const Discord = require('discord.js');
 
 const log = Util.logger;
+    log.defaultPrefix = 'Bot';
 const parseConfig = new Config();
-parseConfig.location = './config/config.yml';
-parseConfig.parse();
-parseConfig.testmode();
-parseConfig.prefill();
+    parseConfig.location = './config/config.dev.yml';
+    parseConfig.parse();
+    parseConfig.testmode();
+    parseConfig.prefill();
+let config = parseConfig.config;
+const language = new Language();
+    language.location = config.language;
+    language.parse();
+let lang = language.language;
+
+const Client = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
+        Discord.Intents.FLAGS.GUILD_BANS,
+        Discord.Intents.FLAGS.GUILD_MEMBERS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_PRESENCES
+    ]
+});
+
+Client.login(config.token);
+
+Client.on('ready', function() {
+    log.log('Client connected!', 'Status');
+    log.log(`\nInvite: ${ createInvite(Client) }\n`, 'Invite');
+
+    Client.on('messageCreate', async function (message) {
+        log.log(message.author.username + ': ' + message.content, 'Message');
+        if(message.content == '!reload') {
+            await message.reply(lang.reload.requested);
+            reload(message);
+        }
+    });
+});
+
+function reload(message) {
+    parseConfig.parse();
+    parseConfig.prefill();
+    config = parseConfig.config;
+    
+    try {
+        Client.destroy();
+    } catch (e) {
+        message.reply(e.message);
+    }
+
+    Client.login(config.token).then(function () {
+        message.reply(lang.reload.success);
+    });
+}
+function createInvite(bot) {
+    return Util.replaceAll(config.inviteFormat, '%id%', bot.user.id);
+}
---
+++ scripts/language.js
@@ -0,0 +1,18 @@
+const Fs = require('fs');
+const Yml = require('yaml');
+const Version = require('./version');
+
+module.exports = function() {
+    this.location = null;
+    this.language = {};
+    this.parse = () => {
+        if (!this.location || this.location == null) throw new Error("No language location specified");
+        let language = Fs.readFileSync(this.location, 'utf8');
+            language = Yml.parse(language);
+
+        if(language.version != Version) throw new Error("Unsupported language version. Version:" + language.version + "; Supported: " + Version);
+
+        this.language = language;
+        return language;
+    };
+}
