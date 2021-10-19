c53ab5c891f968233eb8a3469683007796b61abf
GhexterCortes <69035887+GhexterCortes@users.noreply.github.com>
2021-10-19 21:22:53 +0800

    display version in console

--- scripts/startup.js
+++ scripts/startup.js
@@ -1,8 +1,10 @@
 // Modules
-const { logger } = require('fallout-utility');
+const { logger, loopString } = require('fallout-utility');
+const Version = require('./version');
+
 
 let log = logger;
-        log.defaultPrefix = 'Startup';
+    log.defaultPrefix = 'Startup';
 
 // Export
 module.exports = () => {
@@ -15,5 +17,9 @@ module.exports = () => {
     log.log('  ▒   ▒▒ ░░░   ░▒ ░ ▒ ░░ ░▒  ░ ░');
     log.log('  ░   ▒    ░    ░   ▒ ░░  ░  ░  ');
     log.log('      ░  ░ ░    ░   ░        ░  ');
-                                
+
+    let length = '                                '.length;
+    let version = 'v' + Version;
+    let bar = loopString((length / 2) - (version.length - 2),'=');
+    log.warn(bar + ' ' + version + ' ' + bar);
 }
