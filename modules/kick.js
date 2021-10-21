ee3021e933f95d18e88b4c307736f2151370c739
GhexterCortes <69035887+GhexterCortes@users.noreply.github.com>
2021-10-21 18:50:24 +0800

    ban and kicked

--- modules/kick.js
+++ modules/kick.js
@@ -1,3 +1,5 @@
+const Util = require('fallout-utility');
+const { MessageEmbed } = require('discord.js');
 module.exports = new create();
 
 function create(){
@@ -21,5 +23,28 @@ function create(){
     }
     this.execute = async (args, message, action, client) => {
         // Command executed
+        if(!args.length) { action.reply(message, action.get(this.language.empty)); return; }
+        if(!message.mentions.members.first() || message.mentions.members.first() == null) { action.reply(message, action.get(this.language.needPing)); return; }
+
+        const target = message.mentions.members.first();
+        let reason = Util.makeSentence(args, 1).toString().trim();
+
+        if(reason.length == 0) reason = action.get(this.language.kicked.defaultReason);
+        if(target.user.id == message.author.id) { action.reply(message, action.get(this.language.noPerms)); return; }
+        target.kick({
+            reason: reason
+        }).then((member) => {
+            reason = Util.replaceAll(reason, '%username%', member.username);
+            reason = Util.replaceAll(reason, '%author%', message.author.username);
+
+            let embed = new MessageEmbed()
+                .setTitle(action.get(this.language.kicked.title))
+                .setDescription(reason)
+                .setTimestamp();
+            action.reply(message, { embeds: [embed]});
+        }).catch((err) => {
+            console.error(err);
+            action.reply(message, action.get(this.language.error) + '\n```\n'+ err.message +'\n```');
+        });
     }
 }
