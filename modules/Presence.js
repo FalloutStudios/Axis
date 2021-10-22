f8d54826ee7d49f219243fc7578e2b9ec37bbc9e
GhexterCortes <69035887+GhexterCortes@users.noreply.github.com>
2021-10-22 15:42:49 +0800

    Custom presence!

---
+++ modules/Presence.js
@@ -0,0 +1,23 @@
+// Export the module
+module.exports = new create();
+
+// Create the command
+function create(){
+    // Command and language
+    this.config = {};
+    this.language = {};
+
+    // This is required for both script and command. This will be called when bot is ready or reloaded
+    this.start = (client, action, config, language) => {
+        // Script is ready
+        client.user.setPresence({
+            status: config.presence.status,
+            activities: [{
+                name: config.presence.activityName,
+                type: config.presence.type.toUpperCase()
+            }]
+        });
+
+        return true; // Return true when it's ready
+    }
+}
