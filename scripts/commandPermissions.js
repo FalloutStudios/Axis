067d3cc51f4f7abc852216d3e9c0de29b860a8f5
GhexterCortes <69035887+GhexterCortes@users.noreply.github.com>
2021-11-07 16:26:51 +0800

    parameters definitions fr

--- scripts/commandPermissions.js
+++ scripts/commandPermissions.js
@@ -1,5 +1,15 @@
 const MemberPermission = require('./memberPermissions');
 
+/**
+ * 
+ * @param {string} command - The command name
+ * @param {Object} member - Discord member object
+ * @param {Object[]} list - List of commands with permission levels
+ * @param {boolean} list.enable - Whether the permission level is enabled
+ * @param {Object} list.adminOnlyCommands - List of commands that are only available to admins
+ * @param {Object} list.moderatorOnlyCommands - List of commands that are only available to moderators
+ * @returns 
+ */
 module.exports = (command, member, list) => {
     if(!list.enable) return true;
     if(has(list.adminOnlyCommands, command) && !MemberPermission.admin(member)) return false;
